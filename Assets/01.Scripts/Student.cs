8c6d60a93798e4b46d3acd0b085c0f1bca5e09d3
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-06-09 10:54:52 +0900

    시작

---
+++ Assets/01.Scripts/Student.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Student
+{
+    public string Name;
+    public int Age;
+    public string Gender;
+
+    public override string ToString()
+    {
+        return $"{Name}({Age} {Gender})";
+    }
+
+}
